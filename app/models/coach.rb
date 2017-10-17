d576e1ed7c04e574318bfdb729d700dcb8315005
farzam7777 <l144211@lhr.nu.edu.pk>
2017-10-17 12:56:55 +0500

    Additional attributes added in app controller, paperclip configured, image added for coaches

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -3,4 +3,13 @@ class Coach < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, 
+  default_url: ActionController::Base.helpers.asset_path("missing.jpg")
+
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017073055) do
+ActiveRecord::Schema.define(version: 20171017075552) do
 
   create_table "coaches", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "first_name"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20171017073055) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["email"], name: "index_coaches_on_email", unique: true
     t.index ["reset_password_token"], name: "index_coaches_on_reset_password_token", unique: true
   end
