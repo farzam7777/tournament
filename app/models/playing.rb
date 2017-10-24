8d31072c23f1641d163e7e0bd6f41686b3685713
farzam7777 <l144211@lhr.nu.edu.pk>
2017-10-24 21:46:45 +0500

    Finalizing constraints on team and player count

--- app/models/playing.rb
+++ app/models/playing.rb
@@ -7,12 +7,25 @@ class Playing < ApplicationRecord
 	validates_uniqueness_of :player_id, scope: :sport_id
 	validate :satisfy_playing_option?
 
+	def player_is_present?
+		if self.player.blank?
+			return false
+			errors[:base] << 'You must select one Player. '
+		else
+			return true
+		end
+	end
+
   def satisfy_playing_option?
-    if (self.player.playing_option == 'one') && (self.player.teams.count >= 1)
-    	errors[:base] << self.player.full_name + ' can only play in one team'
-    	return false
-    else
-    	return true
-    end
+  	if player_is_present?
+	    if (self.player.playing_option == 'one') && (self.player.teams.count >= 1)
+	    	errors[:base] << self.player.full_name + ' can only play in one team'
+	    	return false
+	    else
+	    	return true
+	    end
+	  else
+	  	errors[:base] << 'You must Select Player.'
+	  end
   end
 end
